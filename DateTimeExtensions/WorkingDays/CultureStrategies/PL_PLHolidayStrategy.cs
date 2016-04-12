7ead862da29df57849924f20595513774ca78594
MiC11w <mic11w@gmail.com>
2016-04-12 09:52:55 +0200

    Create PL_PLHolidayStrategy.cs

---
+++ DateTimeExtensions/WorkingDays/CultureStrategies/PL_PLHolidayStrategy.cs
@@ -0,0 +1,78 @@
+#region License
+
+// 
+// Copyright (c) 2011-2012, João Matos Silva <kappy@acydburne.com.pt>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DateTimeExtensions.Common;
+
+namespace DateTimeExtensions.WorkingDays.CultureStrategies
+{
+    [Locale("pl-PL")]
+    public class PL_PLHolidayStrategy : HolidayStrategyBase, IHolidayStrategy
+    {
+        public PL_PLHolidayStrategy()
+        {
+            this.InnerHolidays.Add(GlobalHolidays.NewYear);
+            this.InnerHolidays.Add(ChristianHolidays.Epiphany);
+            this.InnerHolidays.Add(ChristianHolidays.Easter);
+            this.InnerHolidays.Add(ChristianHolidays.EasterMonday);
+            this.InnerHolidays.Add(GlobalHolidays.InternationalWorkersDay);
+            this.InnerHolidays.Add(ConstitutionDay);
+            this.InnerHolidays.Add(ChristianHolidays.Pentecost);
+            this.InnerHolidays.Add(ChristianHolidays.CorpusChristi);
+            this.InnerHolidays.Add(ChristianHolidays.Assumption);
+            this.InnerHolidays.Add(ChristianHolidays.AllSaints);
+            this.InnerHolidays.Add(IndependenceDay);
+            this.InnerHolidays.Add(ChristianHolidays.Christmas);
+            this.InnerHolidays.Add(ChristianHolidays.StStephansDay); //drugie swieto
+        }
+
+        private static Holiday constitutionDay;
+
+        public static Holiday ConstitutionDay
+        {
+            get
+            {
+                if (constitutionDay == null)
+                {
+                    constitutionDay = new FixedHoliday("May 3rd Constitution Day", 5, 3);
+                }
+                return constitutionDay;
+            }
+        }
+
+        private static Holiday independenceDay;
+
+        public static Holiday IndependenceDay
+        {
+            get
+            {
+                if (independenceDay == null)
+                {
+                    independenceDay = new FixedHoliday("National Independence Day", 11, 11);
+                }
+                return independenceDay;
+            }
+        }
+    
+    }
+}
